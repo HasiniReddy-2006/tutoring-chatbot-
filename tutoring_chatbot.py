cf139d6c532199371425d0838d01e8495b17dbe4
HasiniReddy-2006 <hasininarsimhareddy2006@gmail.com>
2025-08-02 09:53:33 +0530

    Update tutoring_chatbot.py

--- tutoring_chatbot.py
+++ tutoring_chatbot.py
@@ -50,4 +50,4 @@ with gr.Blocks() as demo:
     msg.submit(chat, [subject, msg, state], [chatbot, state])
     clear.click(lambda: ([], []), None, [chatbot, state])
 
-demo.launch()
+app = demo.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)), prevent_thread_lock=True)
